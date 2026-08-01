5b645cd63342463c554c2e852baef9b70fb91df9
natewest34 <nathanwestlund@outlook.com>
2026-08-01 17:12:25 -0500

    Update config.js with API Keys

--- config.js
+++ config.js
@@ -7,19 +7,11 @@ const CONFIG = {
   // Starting roster. Anyone can add more from the "+ Add a player" link;
   // new names get saved into the QUESTIONS_BIN... no wait, PLAYERS_BIN below.
   PLAYERS: ["Thomas", "Tanner", "Michaela", "Jake", "Nathan"],
-
-  // ---- JSONBin ----
-  // 1. Create a free account at https://jsonbin.io
-  // 2. Grab your X-Master-Key from the API Keys tab
-  // 3. Create THREE bins (content of each can just be `{}` to start):
-  //    - one for daily question sets
-  //    - one for scores
-  //    - one for the player roster
-  //    Paste each bin's ID below.
-  JSONBIN_API_KEY: "PASTE_YOUR_X_MASTER_KEY_HERE",
-  QUESTIONS_BIN_ID: "PASTE_QUESTIONS_BIN_ID_HERE",
-  SCORES_BIN_ID: "PASTE_SCORES_BIN_ID_HERE",
-  PLAYERS_BIN_ID: "PASTE_PLAYERS_BIN_ID_HERE",
+  
+  JSONBIN_API_KEY: "$2a$10$C7Lv1mmqf7swft4ckRdsm.sQ.ysUveuAyJBMlKUYWb0yoSUH8u4xG",
+  QUESTIONS_BIN_ID: "6a6e6eddf5f4af5e29df5e9f",
+  SCORES_BIN_ID: "6a6e6ee6f5f4af5e29df5ec0",
+  PLAYERS_BIN_ID: "6a6e6ef0f5f4af5e29df5edf",
 
   // How many questions per day, split by difficulty.
   DIFFICULTY_MIX: { easy: 3, medium: 4, hard: 3 },
